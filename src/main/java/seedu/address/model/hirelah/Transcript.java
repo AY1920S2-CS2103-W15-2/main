77da48cb6d0951695d004cdc089f404a4294adae
Joash Chin <joashchin@gmail.com>
2020-02-25 20:27:12 +0800

    Create stub model classes in model.hirelah to be implemented

---
+++ src/main/java/seedu/address/model/hirelah/Transcript.java
@@ -0,0 +1,12 @@
+package seedu.address.model.hirelah;
+
+public class Transcript {
+    /*
+    TODO:
+     some way to
+     1. store remark objects
+     2. query by time
+     3. query by question number
+     4. contains the audio file object as well
+     */
+}

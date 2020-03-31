f47df36443a06616a6f9ec80d3046025b0bbd625
agnesnatasya <e0313565@u.nus.edu>
2020-03-31 19:52:51 +0800

    Add test for EditAttributeCommandParser

--- src/test/java/seedu/address/logic/parser/EditAttributeCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/EditAttributeCommandParserTest.java
@@ -1,31 +1,29 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.commands.CommandTestUtility.VALID_INTERVIEWEE_JANE;
-import static seedu.address.logic.commands.CommandTestUtility.VALID_INTERVIEWEE_JANICE;
-import static seedu.address.logic.commands.CommandTestUtility.VALID_PROPERTY_INTERVIEWEE;
-import static seedu.address.logic.commands.CommandTestUtility.WHITESPACE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_NEW;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_OLD;
+import static seedu.address.logic.commands.CommandTestUtility.*;
+import static seedu.address.logic.parser.CliSyntax.*;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 //import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.EditAttributeCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
 
 class EditAttributeCommandParserTest {
 
     private EditAttributeCommandParser parser = new EditAttributeCommandParser();
 
-    //    @Test
-    //    void parse_allFieldsPresent_success() {
-    //        assertParseSuccess(parser, WHITESPACE + VALID_PROPERTY_INTERVIEWEE
-    //                        + WHITESPACE + PREFIX_OLD + WHITESPACE + VALID_INTERVIEWEE_JANE
-    //                        + WHITESPACE + PREFIX_NEW + WHITESPACE + VALID_INTERVIEWEE_JANICE
-    //                        + WHITESPACE,
-    //                new EditAttributeCommand("Jane Doe", "Janice Doe"));
-    //    }
+    @Test
+    void parse_allFieldsPresent_success() {
+
+        assertParseSuccess(parser, WHITESPACE + VALID_ATTRIBUTE_TEAM_WORK
+                        + WHITESPACE + PREFIX_ATTRIBUTE
+                        + WHITESPACE + VALID_ATTRIBUTE_PERSISTENCE,
+                new EditAttributeCommand("team work", "persistence"));
+    }
 
     @Test
     void parse_oldFieldMissing_failure() {

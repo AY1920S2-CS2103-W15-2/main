78dc81bdfb3ee9e0bda9e72455cc8630e16bae1b
agnesnatasya <e0313565@u.nus.edu>
2020-03-31 19:52:09 +0800

    Add checking for EditQuestionCommandParser

--- src/main/java/seedu/address/logic/parser/EditQuestionCommandParser.java
+++ src/main/java/seedu/address/logic/parser/EditQuestionCommandParser.java
@@ -10,7 +10,7 @@ import seedu.address.logic.parser.exceptions.ParseException;
 /**
  * Parses input arguments and creates a new EditQuestionCommand object
  */
-public class EditQuestionCommandParser {
+public class EditQuestionCommandParser implements Parser<EditQuestionCommand> {
 
     /**
      * Parses the given {@code String} of arguments in the context of the EditQuestionCommand
@@ -34,7 +34,7 @@ public class EditQuestionCommandParser {
             int index = Integer.parseInt(argMultimap.getPreamble());
             return new EditQuestionCommand(index, argMultimap.getValue(PREFIX_QUESTION).get());
         } catch (NumberFormatException e) {
-            throw new ParseException(MESSAGE_UNKNOWN_COMMAND);
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, EditQuestionCommand.MESSAGE_USAGE));
         }
     }
 }
